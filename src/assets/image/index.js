a947d7458411777dadef8e1d9891d2708287897d
Aditia Falacha Arvin <ribit@pop-os.localdomain>
2020-08-29 11:06:27 +0700

    register page

---
+++ src/assets/image/index.js
@@ -0,0 +1,4 @@
+import RegisterBg from './register-bg.jpg';
+import LoginBg from './login-bg.jpg';
+
+export {RegisterBg, LoginBg}
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import './register.scss'
+import { RegisterBg } from '../../assets'
+import {Input, Button, Gap, Link} from '../../components'
 
 const Register = () => {
     return (
-        <div>
-            <p>Register</p>
+        <div className='main-page'>
+            <div className='left'>
+                <img className='bg-image' src={RegisterBg} alt='register-background' />
+            </div>
+            <div className='right'>
+                <p className='title'>Register</p>
+                <Input label='Full Name' placeholder='Full Name' />
+                <Gap height={18} />
+                <Input label='Email' placeholder='Email'/>
+                <Gap height={18} />
+                <Input label='Password' placeholder='Password'/>
+                <Gap height={50} />
+                <Button title='Register' />
+                <Gap height={100} />
+                <Link title='Kembali ke login' />
+            </div>
         </div>
     )
 }
