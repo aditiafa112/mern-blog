a51b90b91c9f46a40483a234d407edddd26f9598
Aditia Falacha Arvin <ribit@pop-os.localdomain>
2020-08-30 21:03:07 +0700

    slicing login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,24 @@
 import React from 'react'
+// import './register.scss'
+import { LoginBg } from '../../assets'
+import {Input, Button, Gap, Link} from '../../components'
 
 const Login = () => {
     return (
-        <div>
-            <p>Login Page</p>
+        <div className='main-page'>
+            <div className='left'>
+                <img className='bg-image' src={LoginBg} alt='register-background' />
+            </div>
+            <div className='right'>
+                <p className='title'>Login</p>
+                <Input label='Email' placeholder='Email'/>
+                <Gap height={18} />
+                <Input label='Password' placeholder='Password'/>
+                <Gap height={50} />
+                <Button title='Login' />
+                <Gap height={100} />
+                <Link title='Belum punya akun silakan daftar' />
+            </div>
         </div>
     )
 }
