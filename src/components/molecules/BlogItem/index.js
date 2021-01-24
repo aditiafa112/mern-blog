919a7f7aea5d9b2bdc42857a463bd0c62a352ae6
aditiafa <aditiafa112@gmail.com>
2021-01-24 18:12:09 +0700

    Merubah Value dari State Global Redux

--- src/components/molecules/BlogItem/index.js
+++ src/components/molecules/BlogItem/index.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import { RegisterBg } from '../../../assets'
 import './blogitem.scss'
-import {Button} from "../../../components"
-import {useHistory} from "react-router-dom"
+import { Button } from "../../../components"
+import { useHistory } from "react-router-dom"
 import { Gap } from '../../atoms'
 
-const BlogItem = () => {
+const BlogItem = (props) => {
     const history = useHistory()
+    const { image, title, name, date, body } = props;
     return (
         <div className='blog-item'>
-            <img className='image-thumb' src={RegisterBg} alt="" />
+            <img className='image-thumb' src={image} alt="" />
             <div className='content-detail'>
-                <p className='title'>Title Blog</p>
-                <p className='author'>Author - Date Post</p>
-                <p className='body'>Lorep ipsup dolor </p>
+                <p className='title'>{title}</p>
+                <p className='author'>{name} - {date}</p>
+                <p className='body'>{body}</p>
                 <Gap height={20} />
                 <Button title="View Detail" onClick={() => history.push("/detail-blog")} />
             </div>
-            
+
         </div>
     )
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,30 +1,63 @@
-import React from 'react'
-import { Button, BlogItem, Gap } from '../../components'
-import './home.scss'
-import {useHistory} from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Button, BlogItem, Gap } from "../../components";
+import "./home.scss";
+import { useHistory } from "react-router-dom";
+import Axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
 
 const Home = () => {
-    const history = useHistory();
-    return (
-        <div className='home-page-wrapper'>
-            <div className='create-wrapper'>
-                <Button title='create blog' onClick={() => history.push('/create-blog')} />
-            </div>
-            <Gap height={20} />
-            <div className='content-wrapper'>
-                <BlogItem />
-                <BlogItem />
-                <BlogItem />
-                <BlogItem />
-            </div>
-            <div className='pagination'>
-                <Button title='Previous' />
-                <Gap width={20} />
-                <Button title='Next' />
-            </div>
-            <Gap height={20} />
-        </div>
-    )
-}
+  const { dataBlogs } = useSelector((state) => state);
+  const dispatch = useDispatch();
 
-export default Home
+  useEffect(() => {
+    setTimeout(() => {
+      dispatch({ type: "UPDATE_NAME" })
+    }, 3000);
+
+    Axios.get("http://localhost:4000/v1/blog/posts?page=1&perPage=4")
+      .then((result) => {
+        console.log("data api: ", result.data);
+        const responseApi = result.data;
+
+        // setDataBlog(responseApi.data);
+        dispatch({ type: 'UPDATE_DATA_BLOG', payload: responseApi.data });
+      })
+      .catch((err) => {
+        console.log("error: ", err);
+      });
+  }, []);
+  const history = useHistory();
+  return (
+    <div className="home-page-wrapper">
+      <div className="create-wrapper">
+        <Button
+          title="create blog"
+          onClick={() => history.push("/create-blog")}
+        />
+      </div>
+      <Gap height={20} />
+      <div className="content-wrapper">
+        {
+          dataBlogs.map((blog, index) => {
+            return <BlogItem
+              key={blog._id}
+              image={`http://localhost:4000/${blog.image}`}
+              title={blog.title}
+              body={blog.body}
+              name={blog.author.nama}
+              date={blog.createdAt}
+            />;
+          })
+        }
+      </div>
+      <div className="pagination">
+        <Button title="Previous" />
+        <Gap width={20} />
+        <Button title="Next" />
+      </div>
+      <Gap height={20} />
+    </div>
+  );
+};
+
+export default Home;
